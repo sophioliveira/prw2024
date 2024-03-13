46d088942ad36eacccba4c51c9bb384cc5ddd345
aihpos <sophiapaol06@gmail.com>
2024-03-12 23:13:06 -0300

    lista2

---
+++ LISTA2/1047.php
@@ -0,0 +1,30 @@
+<?php
+list ($HI, $MI, $HF, $MF) = explode (" ", readline());
+if ($HI > $HF && $MI > $MF){
+    echo "O JOGO DUROU ". (24 - $HI + $HF) . " HORA(S) E " . (60 - ($MI - $MF)). " MINUTO(S)\n";
+}
+else if ($HI < $HF && $MI < $MF){
+    echo "O JOGO DUROU " . ($HF - $HI) . " HORA(S) E " . ($MF - $MI) . " MINUTO(S)\n";
+}
+else if ($HI < $HF && $MI > $MF){
+    echo "O JOGO DUROU " . ($HF - $HI - 1) . " HORA(S) E " .(60 - ($MI - $MF)). " MINUTO(S)\n";
+}
+else if ($HI > $HF && $MI < $MF){
+     echo "O JOGO DUROU " . (24 - $HI + $HF) . " HORA(S) E " . ($MF - $MI) . " MINUTO(S)\n";
+}
+else if ($HI < $HF && $MI == $MF){
+    echo "O JOGO DUROU " . ($HF - $HI) . " HORA(S) E 0 MINUTO(S)\n";
+}
+else if ($HI > $HF && $MI == $MF){
+    echo "O JOGO DUROU ". (24 - $HI + $HF) . " HORA(S) E 0 MINUTO(S)\n";
+}
+else if ($HI == $HF && $MI > $MF){
+    echo "O JOGO DUROU 0 HORA(S) E " . (60 - ($MI - $MF)) . " MINUTO(S)\n";
+}
+else if ($HI == $HF && $MI < $MF){
+    echo "O JOGO DUROU 0 HORA(S) E " . ($MF - $MI) . " MINUTO(S)\n";
+}
+else{
+    echo "O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)\n";
+}
+?>
